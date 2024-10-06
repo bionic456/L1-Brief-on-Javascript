daf29040612cd7a21510371bfc7e9bc081234ebc
bionic456 <davidiku60@gmail.com>
2024-10-06 13:54:01 +0100

    Datatypes and conditional statements

---
+++ script.js
@@ -0,0 +1,27 @@
+var age = 13;
+var name = "David";
+var a_student = true;
+var a;
+var b = null;
+
+var movie = {
+    title : "Despicable Me",
+    mc  : "Gru"
+}
+
+var fruits = [
+    "apple", "banana", "papaya"
+]
+
+document.write(typeof(1))
+document.write(fruits[1])
+
+document.write("<br>")
+document.write("<br>")
+
+if (age < 18){
+    document.write("You are a minor" + "<br>")
+}
+else{
+    document.write("You are an adult")
+}
